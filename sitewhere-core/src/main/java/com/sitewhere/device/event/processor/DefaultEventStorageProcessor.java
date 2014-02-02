d0ce436e8849888605c155bd2e93cd61856e8e48
Derek Adams <derek.adams@reveal-tech.com>
2014-02-02 01:13:55 -0500

    Inbound pipeline now accepts registration requests and creates new devices if not already there. Still work to be done on auto-assignment. SITEWHERE-17

--- sitewhere-core/src/main/java/com/sitewhere/device/event/processor/DefaultEventStorageProcessor.java
+++ sitewhere-core/src/main/java/com/sitewhere/device/event/processor/DefaultEventStorageProcessor.java
@@ -9,10 +9,20 @@
  */
 package com.sitewhere.device.event.processor;
 
+import org.apache.log4j.Logger;
+
 import com.sitewhere.rest.model.device.event.processor.InboundEventProcessor;
+import com.sitewhere.rest.model.device.request.DeviceAssignmentCreateRequest;
+import com.sitewhere.rest.model.device.request.DeviceCreateRequest;
+import com.sitewhere.server.SiteWhereServer;
 import com.sitewhere.spi.SiteWhereException;
+import com.sitewhere.spi.SiteWhereSystemException;
+import com.sitewhere.spi.device.IDevice;
+import com.sitewhere.spi.device.IDeviceSpecification;
 import com.sitewhere.spi.device.event.processor.IInboundEventProcessor;
 import com.sitewhere.spi.device.event.request.IDeviceRegistrationRequest;
+import com.sitewhere.spi.error.ErrorCode;
+import com.sitewhere.spi.error.ErrorLevel;
 
 /**
  * Implementation of {@link IInboundEventProcessor} that attempts to store the inbound
@@ -21,6 +31,9 @@ import com.sitewhere.spi.device.event.request.IDeviceRegistrationRequest;
  * @author Derek
  */
 public class DefaultEventStorageProcessor extends InboundEventProcessor {
+
+	/** Static logger instance */
+	private static Logger LOGGER = Logger.getLogger(DefaultEventStorageProcessor.class);
 
 	/*
 	 * (non-Javadoc)
@@ -31,5 +44,34 @@ public class DefaultEventStorageProcessor extends InboundEventProcessor {
 	 */
 	@Override
 	public void onRegistrationRequest(IDeviceRegistrationRequest request) throws SiteWhereException {
+		LOGGER.info("Handling device registration request.");
+		IDevice device =
+				SiteWhereServer.getInstance().getDeviceManagement().getDeviceByHardwareId(
+						request.getHardwareId());
+		IDeviceSpecification specification =
+				SiteWhereServer.getInstance().getDeviceManagement().getDeviceSpecificationByToken(
+						request.getSpecificationToken());
+		// Create device if it does not already exist.
+		if (device == null) {
+			LOGGER.info("Creating new device as part of registration.");
+			if (specification == null) {
+				throw new SiteWhereSystemException(ErrorCode.InvalidDeviceSpecificationToken,
+						ErrorLevel.ERROR);
+			}
+			DeviceCreateRequest deviceCreate = new DeviceCreateRequest();
+			deviceCreate.setHardwareId(request.getHardwareId());
+			deviceCreate.setSpecificationToken(request.getSpecificationToken());
+			deviceCreate.setComments("Device created by on-demand registration.");
+			device = SiteWhereServer.getInstance().getDeviceManagement().createDevice(deviceCreate);
+		} else if (!device.getSpecificationToken().equals(request.getSpecificationToken())) {
+			// TODO: Is this an error or a valid use case?
+			throw new SiteWhereException(
+					"Attempting to register device with different specification that currently assigned");
+		}
+		// Make sure device is assigned.
+		if (device.getAssignmentToken() == null) {
+			LOGGER.info("Handling unassigned device for registration.");
+			DeviceAssignmentCreateRequest assnCreate = new DeviceAssignmentCreateRequest();
+		}
 	}
 }
--- sitewhere-core/src/main/java/com/sitewhere/device/provisioning/DefaultDeviceEventProcessor.java
+++ sitewhere-core/src/main/java/com/sitewhere/device/provisioning/DefaultDeviceEventProcessor.java
@@ -11,14 +11,18 @@ package com.sitewhere.device.provisioning;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.apache.log4j.Logger;
+import org.springframework.security.core.context.SecurityContextHolder;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sitewhere.server.SiteWhereServer;
 import com.sitewhere.spi.SiteWhereException;
 import com.sitewhere.spi.device.event.request.IDeviceEventCreateRequest;
+import com.sitewhere.spi.device.event.request.IDeviceRegistrationRequest;
 import com.sitewhere.spi.device.provisioning.IDeviceEventDecoder;
 import com.sitewhere.spi.device.provisioning.IDeviceEventProcessor;
 import com.sitewhere.spi.device.provisioning.IDeviceEventReceiver;
@@ -33,14 +37,27 @@ public class DefaultDeviceEventProcessor implements IDeviceEventProcessor {
 	/** Static logger instance */
 	private static Logger LOGGER = Logger.getLogger(DefaultDeviceEventProcessor.class);
 
+	/** Maximum size of pending request queue */
+	private static final int MAX_PENDING_REQUEST_QUEUE_SIZE = 100;
+
+	/** Number of threads used for event processing (for all receivers) */
+	private static final int EVENT_PROCESSOR_THREAD_COUNT = 2;
+
 	/** Device event decoder */
 	private IDeviceEventDecoder deviceEventDecoder;
 
 	/** List of {@link IDeviceEventReceiver} that supply this processor */
 	private List<IDeviceEventReceiver> deviceEventReceivers = new ArrayList<IDeviceEventReceiver>();
 
-	/** Used to execute Solr indexing in a separate thread */
-	private ExecutorService pool;
+	/** Thread pool for event receivers */
+	private ExecutorService receiverThreadPool;
+
+	/** Blocking queue of pending event creation requests from receivers */
+	private BlockingQueue<IDeviceEventCreateRequest> pendingRequests =
+			new ArrayBlockingQueue<IDeviceEventCreateRequest>(MAX_PENDING_REQUEST_QUEUE_SIZE);
+
+	/** Thread pool for event creation */
+	private ExecutorService processorThreadPool;
 
 	/*
 	 * (non-Javadoc)
@@ -51,6 +68,7 @@ public class DefaultDeviceEventProcessor implements IDeviceEventProcessor {
 	public void start() throws SiteWhereException {
 		LOGGER.info("Starting device event processing...");
 		startEventReceiverThreads();
+		startEventProcessorThreads();
 		LOGGER.info("Started device event processing.");
 	}
 
@@ -62,13 +80,76 @@ public class DefaultDeviceEventProcessor implements IDeviceEventProcessor {
 	protected void startEventReceiverThreads() throws SiteWhereException {
 		if (getDeviceEventReceivers().size() > 0) {
 			LOGGER.info("Initializing " + getDeviceEventReceivers().size() + " device event receivers...");
-			pool = Executors.newFixedThreadPool(getDeviceEventReceivers().size());
+			receiverThreadPool = Executors.newFixedThreadPool(getDeviceEventReceivers().size());
 			for (IDeviceEventReceiver receiver : getDeviceEventReceivers()) {
 				receiver.start();
-				pool.execute(new EventReceiverProcessor(receiver));
+				receiverThreadPool.execute(new EventReceiverProcessor(receiver));
 			}
 		} else {
 			LOGGER.warn("No device event receivers configured for processor!");
+		}
+	}
+
+	/**
+	 * Create threads that will process {@link IDeviceEventCreateRequest} requests for all
+	 * receivers.
+	 * 
+	 * @throws SiteWhereException
+	 */
+	protected void startEventProcessorThreads() throws SiteWhereException {
+		LOGGER.info("Initializing " + EVENT_PROCESSOR_THREAD_COUNT + " device event processors...");
+		processorThreadPool = Executors.newFixedThreadPool(EVENT_PROCESSOR_THREAD_COUNT);
+		for (int i = 0; i < EVENT_PROCESSOR_THREAD_COUNT; i++) {
+			processorThreadPool.execute(new EventProcessor());
+		}
+	}
+
+	/**
+	 * Thread that processes events being decoded from {@link IDeviceEventReceiver}
+	 * threads.
+	 * 
+	 * @author Derek
+	 */
+	private class EventProcessor implements Runnable {
+
+		@Override
+		public void run() {
+			LOGGER.info("Started inbound event processor thread.");
+
+			// Event creation APIs expect an authenticated user in order to check
+			// permissions and log who creates events. When called in this context, the
+			// authenticated user will always be 'system'.
+			//
+			// TODO: Alternatively, we may want the client to authenticate on registration
+			// and pass a token on each request.
+			try {
+				SecurityContextHolder.getContext().setAuthentication(
+						SiteWhereServer.getSystemAuthentication());
+			} catch (SiteWhereException e) {
+				throw new RuntimeException(
+						"Unable to use system authentication for inbound event processor thread.", e);
+			}
+			while (true) {
+				try {
+					IDeviceEventCreateRequest request = pendingRequests.take();
+					LOGGER.debug("Device event processor thread picked up request.");
+					if (request instanceof IDeviceRegistrationRequest) {
+						LOGGER.debug("Processing registration request.");
+						SiteWhereServer.getInstance().getInboundEventProcessorChain().onRegistrationRequest(
+								(IDeviceRegistrationRequest) request);
+					} else {
+						throw new SiteWhereException(
+								"Unknown request type decoded by inbound receiver. Type was: "
+										+ request.getClass().getName());
+					}
+				} catch (SiteWhereException e) {
+					LOGGER.error("Event receiver thread unable to decode event request.", e);
+				} catch (InterruptedException e) {
+					LOGGER.warn("Device event receiver thread interrupted.", e);
+				} catch (Throwable e) {
+					LOGGER.error("Unhandled exception in device event decoding.", e);
+				}
+			}
 		}
 	}
 
@@ -88,25 +169,20 @@ public class DefaultDeviceEventProcessor implements IDeviceEventProcessor {
 
 		@Override
 		public void run() {
-			LOGGER.info("Started device provisioning processor thread.");
+			LOGGER.info("Started device provisioning receiver thread.");
 			while (true) {
 				try {
 					byte[] message = receiver.getEncodedMessages().take();
-					try {
-						LOGGER.debug("Device event processor thread picked up event.");
-						IDeviceEventCreateRequest request = getDeviceEventDecoder().decode(message);
-						ObjectMapper mapper = new ObjectMapper();
-						String output = mapper.writeValueAsString(request);
-						LOGGER.info(output);
-					} catch (SiteWhereException e) {
-						LOGGER.error("Unable to decode inbound device event.", e);
-					} catch (Throwable e) {
-						LOGGER.error("Unhandled exception in device event decoding.", e);
-					}
+					LOGGER.debug("Device event receiver thread picked up event.");
+					IDeviceEventCreateRequest request = getDeviceEventDecoder().decode(message);
+					pendingRequests.offer(request);
+					LOGGER.debug("Decoded event added to processing queue.");
 				} catch (SiteWhereException e) {
-					LOGGER.warn("Device event processor interrupted.", e);
+					LOGGER.error("Event receiver thread unable to decode event request.", e);
 				} catch (InterruptedException e) {
-					LOGGER.error(e);
+					LOGGER.warn("Device event receiver thread interrupted.", e);
+				} catch (Throwable e) {
+					LOGGER.error("Unhandled exception in device event decoding.", e);
 				}
 			}
 		}
