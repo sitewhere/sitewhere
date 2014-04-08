0189f15a3039c30acf5003aba16a84f83194e00c
Derek Adams <derek.adams@reveal-tech.com>
2014-04-08 12:48:57 -0400

    More work on GNU health asset modules. Factored out common configuration logic for reuse between modules. SITEWHERE-36

--- sitewhere-gnuhealth/src/main/java/com/sitewhere/gnuhealth/GnuHealthWardsAssetModule.java
+++ sitewhere-gnuhealth/src/main/java/com/sitewhere/gnuhealth/GnuHealthWardsAssetModule.java
@@ -9,14 +9,32 @@
  */
 package com.sitewhere.gnuhealth;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.jaxrs.client.WebClient;
 import org.apache.log4j.Logger;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import com.sitewhere.rest.model.asset.HardwareAsset;
+import com.sitewhere.rest.model.asset.LocationAsset;
+import com.sitewhere.rest.model.command.CommandResponse;
+import com.sitewhere.server.asset.scim.IScimFields;
 import com.sitewhere.spi.SiteWhereException;
 import com.sitewhere.spi.asset.AssetType;
 import com.sitewhere.spi.asset.IAssetModule;
+import com.sitewhere.spi.command.CommandResult;
 import com.sitewhere.spi.command.ICommandResponse;
 
 /**
@@ -33,16 +51,25 @@ public class GnuHealthWardsAssetModule implements IAssetModule<HardwareAsset> {
 	private static final String MODULE_ID = "gnuhealth-wards";
 
 	/** Module name */
-	private static final String MODULE_NAME = "GNU Health Wards Asset Module";
+	private static final String MODULE_NAME = "GNU Health Wards";
 
-	/** Default base URL Tryton JSON-RPC responder */
-	private static final String DEFAULT_URL = "http://sitewhere-aws:8000/gnuhealth";
+	/** Unique module id */
+	private String moduleId = MODULE_ID;
 
-	/** Default Tryton username */
-	private static final String DEFAULT_USERNAME = "admin";
+	/** Module name */
+	private String moduleName = MODULE_NAME;
 
-	/** Default Tryton password */
-	private static final String DEFAULT_PASSWORD = "admin";
+	/** Common GNU Health configuration */
+	private GnuHealthConfiguration configuration;
+
+	/** Use CXF web client to send requests */
+	private WebClient client;
+
+	/** Jackson JSON mapper */
+	private ObjectMapper mapper = new ObjectMapper();
+
+	/** Cached asset map */
+	private Map<String, LocationAsset> assetCache = new HashMap<String, LocationAsset>();
 
 	/*
 	 * (non-Javadoc)
@@ -51,6 +78,14 @@ public class GnuHealthWardsAssetModule implements IAssetModule<HardwareAsset> {
 	 */
 	@Override
 	public void start() throws SiteWhereException {
+		LOGGER.info("Connecting to GNU Health instance at: " + getConfiguration().getBaseUrl());
+		List<Object> providers = new ArrayList<Object>();
+		providers.add(new JacksonJsonProvider());
+		this.client =
+				WebClient.create(getConfiguration().getBaseUrl(), providers).type(MediaType.APPLICATION_JSON).accept(
+						MediaType.APPLICATION_JSON);
+		getConfiguration().login(client);
+		// cacheAssetData();
 	}
 
 	/*
@@ -62,34 +97,56 @@ public class GnuHealthWardsAssetModule implements IAssetModule<HardwareAsset> {
 	public void stop() throws SiteWhereException {
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Cache asset information from the GNU Health server.
 	 * 
-	 * @see com.sitewhere.spi.asset.IAssetModule#getId()
+	 * @return
+	 * @throws SiteWhereException
 	 */
-	@Override
-	public String getId() {
-		return MODULE_ID;
+	protected ICommandResponse cacheAssetData() throws SiteWhereException {
+		assetCache.clear();
+
+		LOGGER.info("Caching search data.");
+		int totalAssets = 0;
+		long startTime = System.currentTimeMillis();
+		Response response = client.get();
+		Object entity = response.getEntity();
+		try {
+			JsonNode json = mapper.readTree((InputStream) entity);
+			JsonNode resources = json.get(IScimFields.RESOURCES);
+			if (resources == null) {
+				String message = "SCIM JSON response did not contain a 'resources' section.";
+				LOGGER.info(message);
+				return new CommandResponse(CommandResult.Failed, message);
+			}
+			Iterator<JsonNode> it = resources.elements();
+			while (it.hasNext()) {
+				JsonNode resource = it.next();
+				LocationAsset asset = parse(resource);
+				assetCache.put(asset.getId(), asset);
+				totalAssets++;
+			}
+		} catch (JsonParseException e) {
+			throw new SiteWhereException("Unable to parse asset response.", e);
+		} catch (IOException e) {
+			throw new SiteWhereException("Unable to read asset response.", e);
+		}
+		long totalTime = System.currentTimeMillis() - startTime;
+		String message = "Cached " + totalAssets + " assets in " + totalTime + "ms.";
+		LOGGER.info(message);
+		return new CommandResponse(CommandResult.Successful, message);
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Parse JSON response into an asset record.
 	 * 
-	 * @see com.sitewhere.spi.asset.IAssetModule#getName()
+	 * @param resource
+	 * @return
+	 * @throws SiteWhereException
 	 */
-	@Override
-	public String getName() {
-		return MODULE_NAME;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.sitewhere.spi.asset.IAssetModule#getAssetType()
-	 */
-	@Override
-	public AssetType getAssetType() {
-		return AssetType.Hardware;
+	protected LocationAsset parse(JsonNode resource) throws SiteWhereException {
+		LocationAsset asset = new LocationAsset();
+		return asset;
 	}
 
 	/*
@@ -120,5 +177,59 @@ public class GnuHealthWardsAssetModule implements IAssetModule<HardwareAsset> {
 	@Override
 	public ICommandResponse refresh() throws SiteWhereException {
 		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.sitewhere.spi.asset.IAssetModule#getId()
+	 */
+	@Override
+	public String getId() {
+		return getModuleId();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.sitewhere.spi.asset.IAssetModule#getName()
+	 */
+	@Override
+	public String getName() {
+		return getModuleName();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.sitewhere.spi.asset.IAssetModule#getAssetType()
+	 */
+	@Override
+	public AssetType getAssetType() {
+		return AssetType.Location;
+	}
+
+	public String getModuleId() {
+		return moduleId;
+	}
+
+	public void setModuleId(String moduleId) {
+		this.moduleId = moduleId;
+	}
+
+	public String getModuleName() {
+		return moduleName;
+	}
+
+	public void setModuleName(String moduleName) {
+		this.moduleName = moduleName;
+	}
+
+	public GnuHealthConfiguration getConfiguration() {
+		return configuration;
+	}
+
+	public void setConfiguration(GnuHealthConfiguration configuration) {
+		this.configuration = configuration;
 	}
 }
