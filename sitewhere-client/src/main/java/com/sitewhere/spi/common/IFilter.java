09a0580d3e88f650142a09ea51709ac4ceb78b16
Derek Adams <derek.adams@reveal-tech.com>
2014-04-24 00:27:16 -0400

    Added filter interface for narrowing list operations. SITEWHERE-34

---
+++ sitewhere-client/src/main/java/com/sitewhere/spi/common/IFilter.java
@@ -0,0 +1,26 @@
+/*
+ * IFilter.java 
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) Reveal Technologies, LLC. All rights reserved. http://www.reveal-tech.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package com.sitewhere.spi.common;
+
+/**
+ * Determines whether a record should be filtered from a result set.
+ * 
+ * @author Derek
+ */
+public interface IFilter<T> {
+
+	/**
+	 * Indicates whether the item is excluded.
+	 * 
+	 * @param item
+	 * @return
+	 */
+	public boolean isExcluded(T item);
+}
