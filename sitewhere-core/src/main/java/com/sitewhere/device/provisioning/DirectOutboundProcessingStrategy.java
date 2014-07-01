91b88dac1b0c90941b5487b24132e86e506f9d4b
Derek Adams <derek.adams@reveal-tech.com>
2014-07-01 19:18:16 -0400

    Add strategy that sends events directly to the outbound processing chain. SITEWHERE-54

---
+++ sitewhere-core/src/main/java/com/sitewhere/device/provisioning/DirectOutboundProcessingStrategy.java
@@ -0,0 +1,107 @@
+/*
+ * DirectOutboundProcessingStrategy.java 
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) Reveal Technologies, LLC. All rights reserved. http://www.reveal-tech.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package com.sitewhere.device.provisioning;
+
+import com.sitewhere.server.SiteWhereServer;
+import com.sitewhere.spi.SiteWhereException;
+import com.sitewhere.spi.device.event.IDeviceAlert;
+import com.sitewhere.spi.device.event.IDeviceCommandInvocation;
+import com.sitewhere.spi.device.event.IDeviceCommandResponse;
+import com.sitewhere.spi.device.event.IDeviceLocation;
+import com.sitewhere.spi.device.event.IDeviceMeasurements;
+import com.sitewhere.spi.device.event.processor.IOutboundEventProcessorChain;
+import com.sitewhere.spi.device.provisioning.IOutboundProcessingStrategy;
+
+/**
+ * Implementation of {@link IOutboundProcessingStrategy} that sends messages directly to
+ * the {@link IOutboundEventProcessorChain}.
+ * 
+ * @author Derek
+ */
+public class DirectOutboundProcessingStrategy implements IOutboundProcessingStrategy {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.sitewhere.spi.ISiteWhereLifecycle#start()
+	 */
+	@Override
+	public void start() throws SiteWhereException {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.sitewhere.spi.ISiteWhereLifecycle#stop()
+	 */
+	@Override
+	public void stop() throws SiteWhereException {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.sitewhere.spi.device.event.processor.IOutboundEventProcessor#onMeasurements
+	 * (com.sitewhere.spi.device.event.IDeviceMeasurements)
+	 */
+	@Override
+	public void onMeasurements(IDeviceMeasurements measurements) throws SiteWhereException {
+		SiteWhereServer.getInstance().getOutboundEventProcessorChain().onMeasurements(measurements);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.sitewhere.spi.device.event.processor.IOutboundEventProcessor#onLocation(com
+	 * .sitewhere.spi.device.event.IDeviceLocation)
+	 */
+	@Override
+	public void onLocation(IDeviceLocation location) throws SiteWhereException {
+		SiteWhereServer.getInstance().getOutboundEventProcessorChain().onLocation(location);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.sitewhere.spi.device.event.processor.IOutboundEventProcessor#onAlert(com.sitewhere
+	 * .spi.device.event.IDeviceAlert)
+	 */
+	@Override
+	public void onAlert(IDeviceAlert alert) throws SiteWhereException {
+		SiteWhereServer.getInstance().getOutboundEventProcessorChain().onAlert(alert);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.sitewhere.spi.device.event.processor.IOutboundEventProcessor#onCommandInvocation
+	 * (com.sitewhere.spi.device.event.IDeviceCommandInvocation)
+	 */
+	@Override
+	public void onCommandInvocation(IDeviceCommandInvocation invocation) throws SiteWhereException {
+		SiteWhereServer.getInstance().getOutboundEventProcessorChain().onCommandInvocation(invocation);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.sitewhere.spi.device.event.processor.IOutboundEventProcessor#onCommandResponse
+	 * (com.sitewhere.spi.device.event.IDeviceCommandResponse)
+	 */
+	@Override
+	public void onCommandResponse(IDeviceCommandResponse response) throws SiteWhereException {
+		SiteWhereServer.getInstance().getOutboundEventProcessorChain().onCommandResponse(response);
+	}
+}
