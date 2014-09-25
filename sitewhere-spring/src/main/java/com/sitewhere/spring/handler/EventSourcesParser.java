4ea87e7ffc79f7afa87c83858be14606f35e7ec2
Derek Adams <derek.adams@reveal-tech.com>
2014-09-25 15:55:53 -0400

    Added schema support for simplified MQTT event source. SITEWHERE-70

--- sitewhere-spring/src/main/java/com/sitewhere/spring/handler/EventSourcesParser.java
+++ sitewhere-spring/src/main/java/com/sitewhere/spring/handler/EventSourcesParser.java
@@ -12,12 +12,17 @@ package com.sitewhere.spring.handler;
 import java.util.List;
 
 import org.springframework.beans.factory.config.RuntimeBeanReference;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultBeanNameGenerator;
 import org.springframework.beans.factory.support.ManagedList;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.springframework.util.xml.DomUtils;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Element;
 
+import com.sitewhere.device.provisioning.BinaryInboundEventSource;
+import com.sitewhere.device.provisioning.mqtt.MqttInboundEventReceiver;
 import com.sitewhere.spi.device.provisioning.IInboundEventSource;
 
 /**
@@ -26,6 +31,9 @@ import com.sitewhere.spi.device.provisioning.IInboundEventSource;
  * @author Derek
  */
 public class EventSourcesParser {
+
+	/** Used to generate unique names for nested beans */
+	private DefaultBeanNameGenerator nameGenerator = new DefaultBeanNameGenerator();
 
 	/**
 	 * Parse the list of event sources.
@@ -45,6 +53,10 @@ public class EventSourcesParser {
 			switch (type) {
 			case EventSource: {
 				result.add(parseEventSource(child, context));
+				break;
+			}
+			case MqttEventSource: {
+				result.add(parseMqttEventSource(child, context));
 				break;
 			}
 			}
@@ -68,6 +80,72 @@ public class EventSourcesParser {
 	}
 
 	/**
+	 * Parse an MQTT event source.
+	 * 
+	 * @param element
+	 * @param context
+	 * @return
+	 */
+	protected AbstractBeanDefinition parseMqttEventSource(Element element, ParserContext context) {
+		BeanDefinitionBuilder source =
+				BeanDefinitionBuilder.rootBeanDefinition(BinaryInboundEventSource.class);
+
+		// Create MQTT event receiver bean and register it.
+		AbstractBeanDefinition receiver = createMqttEventReceiver(element);
+		String receiverName = nameGenerator.generateBeanName(receiver, context.getRegistry());
+		context.getRegistry().registerBeanDefinition(receiverName, receiver);
+
+		// Create list with bean reference and add it as property.
+		ManagedList<Object> list = new ManagedList<Object>();
+		RuntimeBeanReference ref = new RuntimeBeanReference(receiverName);
+		list.add(ref);
+		source.addPropertyValue("inboundEventReceivers", list);
+
+		// Add decoder reference.
+		Element decoder = DomUtils.getChildElementByTagName(element, "decoder");
+		if (decoder == null) {
+			throw new RuntimeException("Command decoder required but not specified.");
+		}
+		Attr decoderRef = decoder.getAttributeNode("ref");
+		if (decoderRef == null) {
+			throw new RuntimeException("Command decoder ref required but not specified.");
+		}
+		source.addPropertyReference("deviceEventDecoder", decoderRef.getValue());
+
+		return source.getBeanDefinition();
+	}
+
+	/**
+	 * Create MQTT event receiver from XML element.
+	 * 
+	 * @param element
+	 * @return
+	 */
+	protected AbstractBeanDefinition createMqttEventReceiver(Element element) {
+		BeanDefinitionBuilder mqtt = BeanDefinitionBuilder.rootBeanDefinition(MqttInboundEventReceiver.class);
+
+		Attr hostname = element.getAttributeNode("hostname");
+		if (hostname == null) {
+			throw new RuntimeException("MQTT hostname attribute not provided.");
+		}
+		mqtt.addPropertyValue("hostname", hostname.getValue());
+
+		Attr port = element.getAttributeNode("port");
+		if (port == null) {
+			throw new RuntimeException("MQTT port attribute not provided.");
+		}
+		mqtt.addPropertyValue("port", port.getValue());
+
+		Attr topic = element.getAttributeNode("topic");
+		if (topic == null) {
+			throw new RuntimeException("MQTT topic attribute not provided.");
+		}
+		mqtt.addPropertyValue("topic", topic.getValue());
+
+		return mqtt.getBeanDefinition();
+	}
+
+	/**
 	 * Expected child elements.
 	 * 
 	 * @author Derek
@@ -75,7 +153,10 @@ public class EventSourcesParser {
 	public static enum Elements {
 
 		/** Event source */
-		EventSource("event-source");
+		EventSource("event-source"),
+
+		/** Event source */
+		MqttEventSource("mqtt-event-source");
 
 		/** Event code */
 		private String localName;
