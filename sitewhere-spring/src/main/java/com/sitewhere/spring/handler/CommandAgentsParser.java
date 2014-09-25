30dcc708cd83548f59fd9d0e1731ffdd773cec5e
Derek Adams <derek.adams@reveal-tech.com>
2014-09-25 11:09:08 -0400

    Added schema support for Twilio command agent. SITEWHERE-70

--- sitewhere-spring/src/main/java/com/sitewhere/spring/handler/CommandAgentsParser.java
+++ sitewhere-spring/src/main/java/com/sitewhere/spring/handler/CommandAgentsParser.java
@@ -12,12 +12,16 @@ package com.sitewhere.spring.handler;
 import java.util.List;
 
 import org.springframework.beans.factory.config.RuntimeBeanReference;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultBeanNameGenerator;
 import org.springframework.beans.factory.support.ManagedList;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.springframework.util.xml.DomUtils;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Element;
 
+import com.sitewhere.device.provisioning.sms.SmsOutboundCommandAgent;
 import com.sitewhere.spi.device.provisioning.IOutboundCommandAgent;
 
 /**
@@ -26,6 +30,9 @@ import com.sitewhere.spi.device.provisioning.IOutboundCommandAgent;
  * @author Derek
  */
 public class CommandAgentsParser {
+
+	/** Used to generate unique names for nested beans */
+	private DefaultBeanNameGenerator nameGenerator = new DefaultBeanNameGenerator();
 
 	/**
 	 * Parse the list of command agents.
@@ -47,6 +54,10 @@ public class CommandAgentsParser {
 				result.add(parseCommandAgentReference(child, context));
 				break;
 			}
+			case TwilioCommandAgent: {
+				result.add(parseTwilioCommandAgent(child, context));
+				break;
+			}
 			}
 		}
 		return result;
@@ -64,7 +75,85 @@ public class CommandAgentsParser {
 		if (ref != null) {
 			return new RuntimeBeanReference(ref.getValue());
 		}
-		throw new RuntimeException("Command agent reference does not have ref defined.");
+		throw new RuntimeException("Command agent reference not contain ref attribute.");
+	}
+
+	/**
+	 * Parse the Twilio command agent configuration and create beans necessary for
+	 * implementation.
+	 * 
+	 * @param element
+	 * @param context
+	 * @return
+	 */
+	protected AbstractBeanDefinition parseTwilioCommandAgent(Element element, ParserContext context) {
+		BeanDefinitionBuilder sms = BeanDefinitionBuilder.rootBeanDefinition(SmsOutboundCommandAgent.class);
+		Attr agentId = element.getAttributeNode("agentId");
+		if (agentId == null) {
+			throw new RuntimeException("Command agent does not contain agentId attribute.");
+		}
+		sms.addPropertyValue("agentId", agentId.getValue());
+
+		// Add Twilio command delivery provider bean.
+		AbstractBeanDefinition twilioDef = createTwilioDeliveryProvider(element);
+		String twilioName = nameGenerator.generateBeanName(twilioDef, context.getRegistry());
+		context.getRegistry().registerBeanDefinition(twilioName, twilioDef);
+		sms.addPropertyReference("commandDeliveryProvider", twilioName);
+
+		// Locate encoder reference.
+		Element encoder = DomUtils.getChildElementByTagName(element, "encoder");
+		if (encoder == null) {
+			throw new RuntimeException("Command encoder required but not specified.");
+		}
+		Attr ref = encoder.getAttributeNode("ref");
+		if (ref == null) {
+			throw new RuntimeException("Command encoder ref required but not specified.");
+		}
+		sms.addPropertyReference("commandExecutionEncoder", ref.getValue());
+
+		// Locate parameter extractor reference.
+		Element extractor = DomUtils.getChildElementByTagName(element, "parameter-extractor");
+		if (extractor == null) {
+			throw new RuntimeException("Parameter extractor required but not specified.");
+		}
+		Attr pref = extractor.getAttributeNode("ref");
+		if (pref == null) {
+			throw new RuntimeException("Parameter extractor ref required but not specified.");
+		}
+		sms.addPropertyReference("commandDeliveryParameterExtractor", pref.getValue());
+
+		return sms.getBeanDefinition();
+	}
+
+	/**
+	 * Create Twilio command delivery provider from XML element.
+	 * 
+	 * @param element
+	 * @return
+	 */
+	protected AbstractBeanDefinition createTwilioDeliveryProvider(Element element) {
+		BeanDefinitionBuilder twilio =
+				BeanDefinitionBuilder.rootBeanDefinition("com.sitewhere.twilio.TwilioCommandDeliveryProvider");
+
+		Attr accountSid = element.getAttributeNode("accountSid");
+		if (accountSid == null) {
+			throw new RuntimeException("Twilio account SID attribute not provided.");
+		}
+		twilio.addPropertyValue("accountSid", accountSid.getValue());
+
+		Attr authToken = element.getAttributeNode("authToken");
+		if (authToken == null) {
+			throw new RuntimeException("Twilio auth token attribute not provided.");
+		}
+		twilio.addPropertyValue("authToken", authToken.getValue());
+
+		Attr fromPhoneNumber = element.getAttributeNode("fromPhoneNumber");
+		if (fromPhoneNumber == null) {
+			throw new RuntimeException("Twilio from phone number attribute not provided.");
+		}
+		twilio.addPropertyValue("fromPhoneNumber", fromPhoneNumber.getValue());
+
+		return twilio.getBeanDefinition();
 	}
 
 	/**
@@ -75,7 +164,10 @@ public class CommandAgentsParser {
 	public static enum Elements {
 
 		/** Command agent */
-		CommandAgent("command-agent");
+		CommandAgent("command-agent"),
+
+		/** Twilio command agent */
+		TwilioCommandAgent("twilio-command-agent");
 
 		/** Event code */
 		private String localName;
