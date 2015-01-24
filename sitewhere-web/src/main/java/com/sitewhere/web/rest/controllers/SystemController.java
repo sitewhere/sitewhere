a2c503b588e4de0daecdfb90e2ea721bd982507c
Derek Adams <derek.adams@reveal-tech.com>
2015-01-24 16:33:34 -0500

    Removed system REST calls that are not used by community edition.

--- sitewhere-web/src/main/java/com/sitewhere/web/rest/controllers/SystemController.java
+++ sitewhere-web/src/main/java/com/sitewhere/web/rest/controllers/SystemController.java
@@ -10,28 +10,18 @@ package com.sitewhere.web.rest.controllers;
 import org.apache.log4j.Logger;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sitewhere.SiteWhere;
 import com.sitewhere.Tracer;
 import com.sitewhere.core.user.SitewhereRoles;
-import com.sitewhere.rest.model.cache.CacheInformation;
-import com.sitewhere.rest.model.cache.CacheSummary;
 import com.sitewhere.spi.SiteWhereException;
-import com.sitewhere.spi.cache.ICache;
-import com.sitewhere.spi.device.IDeviceManagementCacheProvider;
-import com.sitewhere.spi.server.debug.ITracer;
 import com.sitewhere.spi.server.debug.TracerCategory;
-import com.sitewhere.spi.server.lifecycle.ILifecycleComponent;
 import com.sitewhere.spi.system.IVersion;
-import com.sitewhere.spi.system.SystemComponents;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
 
 /**
  * Controller for system operations.
@@ -40,7 +30,7 @@ import com.wordnik.swagger.annotations.ApiParam;
  */
 @Controller
 @RequestMapping(value = "/system")
-@Api(value = "system", description = "Operations related to SiteWhere system management.")
+@Api(value = "system", description = "Operations related to SiteWhere CE system management.")
 public class SystemController extends SiteWhereController {
 
 	/** Static logger instance */
@@ -54,112 +44,6 @@ public class SystemController extends SiteWhereController {
 		Tracer.start(TracerCategory.RestApiCall, "getVersion", LOGGER);
 		try {
 			return SiteWhere.getServer().getVersion();
-		} finally {
-			Tracer.stop(LOGGER);
-		}
-	}
-
-	@RequestMapping(value = "/component/{shortName}/lifecycle/{command}", method = RequestMethod.PUT)
-	@ResponseBody
-	@ApiOperation(value = "Request component lifecycle change.")
-	@Secured({ SitewhereRoles.ROLE_AUTHENTICATED_USER })
-	public void updateComponentLifecycle(
-			@ApiParam(value = "Component short name", required = true) @PathVariable String shortName,
-			@ApiParam(value = "Lifecycle command", required = true) @PathVariable String command)
-			throws SiteWhereException {
-		Tracer.start(TracerCategory.RestApiCall, "updateComponentLifecycle", LOGGER);
-		try {
-			SystemComponents component = SystemComponents.getByShortName(shortName);
-			if (component == null) {
-				throw new SiteWhereException("Unknown lifecycle component.");
-			}
-			ILifecycleComponent lifecycle = null;
-			switch (component) {
-			case BatchOperationManager: {
-				lifecycle = SiteWhere.getServer().getDeviceProvisioning().getBatchOperationManager();
-				break;
-			}
-			case DeviceProvisioning: {
-				lifecycle = SiteWhere.getServer().getDeviceProvisioning();
-				break;
-			}
-			}
-			if (command.equalsIgnoreCase("start")) {
-				lifecycle.lifecycleStart();
-			} else if (command.equalsIgnoreCase("stop")) {
-				lifecycle.lifecycleStop();
-			} else if (command.equalsIgnoreCase("pause")) {
-				lifecycle.lifecyclePause();
-			}
-		} finally {
-			Tracer.stop(LOGGER);
-		}
-	}
-
-	/**
-	 * Get a summary of statistics for caches.
-	 * 
-	 * @return
-	 * @throws SiteWhereException
-	 */
-	@RequestMapping(value = "/cachesummary", method = RequestMethod.GET)
-	@ResponseBody
-	@ApiOperation(value = "Get a summary of cache statistics")
-	@Secured({ SitewhereRoles.ROLE_AUTHENTICATED_USER })
-	public CacheSummary getCacheSummary() throws SiteWhereException {
-		Tracer.start(TracerCategory.RestApiCall, "getCacheSummary", LOGGER);
-		try {
-			CacheSummary summary = new CacheSummary();
-			IDeviceManagementCacheProvider provider =
-					SiteWhere.getServer().getDeviceManagementCacheProvider();
-			if (provider != null) {
-				summary.getCaches().add(getCacheInformation(provider.getSiteCache()));
-				summary.getCaches().add(getCacheInformation(provider.getDeviceSpecificationCache()));
-				summary.getCaches().add(getCacheInformation(provider.getDeviceCache()));
-				summary.getCaches().add(getCacheInformation(provider.getDeviceAssignmentCache()));
-			}
-			return summary;
-		} finally {
-			Tracer.stop(LOGGER);
-		}
-	}
-
-	/**
-	 * Extract information from cache.
-	 * 
-	 * @param cache
-	 * @return
-	 * @throws SiteWhereException
-	 */
-	protected CacheInformation getCacheInformation(ICache<?, ?> cache) throws SiteWhereException {
-		CacheInformation info = new CacheInformation();
-		info.setCacheType(cache.getType());
-		info.setRequestCount(cache.getRequestCount());
-		info.setHitCount(cache.getHitCount());
-		if (cache.getRequestCount() > 0) {
-			info.setHitRatio((double) cache.getHitCount() / (double) cache.getRequestCount());
-		} else {
-			info.setHitRatio(0);
-		}
-		return info;
-	}
-
-	/**
-	 * Set enablement of {@link ITracer} implementation.
-	 * 
-	 * @param enabled
-	 * @throws SiteWhereException
-	 */
-	@RequestMapping(value = "/tracer", method = RequestMethod.POST)
-	@ResponseBody
-	@ApiOperation(value = "Set tracer enablement")
-	@Secured({ SitewhereRoles.ROLE_AUTHENTICATED_USER })
-	public void setTracerEnablement(
-			@ApiParam(value = "Enable tracer implementation", required = false) @RequestParam(defaultValue = "true") boolean enabled)
-			throws SiteWhereException {
-		Tracer.start(TracerCategory.RestApiCall, "setTracerEnablement", LOGGER);
-		try {
-			Tracer.setEnabled(enabled);
 		} finally {
 			Tracer.stop(LOGGER);
 		}
