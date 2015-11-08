82840132811c4367523bed9464dbf6f3ccee3066
Derek Adams <derek.adams@sitewhere.com>
2015-11-08 10:39:39 -0500

    Fixes for pull request.

--- sitewhere-wso2/src/main/java/com/sitewhere/wso2/identity/ksoap2/Wso2SoapClient.java
+++ sitewhere-wso2/src/main/java/com/sitewhere/wso2/identity/ksoap2/Wso2SoapClient.java
@@ -9,6 +9,8 @@ package com.sitewhere.wso2.identity.ksoap2;
 
 import java.io.IOException;
 import java.net.Authenticator;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
@@ -27,9 +29,6 @@ import org.ksoap2.transport.ServiceConnection;
 import org.xmlpull.v1.XmlPullParserException;
 
 import com.sitewhere.spi.SiteWhereException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Vector;
 
 /**
  * Client for WSO2 remote user store manager service using ksoap2 for lightweight SOAP
@@ -81,13 +80,13 @@ public class Wso2SoapClient {
 		Object response = send("urn:authenticate", body);
 		return getPrimitiveBoolean(response);
 	}
-        
-        public List<String> getRoleListOfUser(String username) throws SiteWhereException {
-            SoapObject body = new SoapObject(CARBON_NAMESPACE, "getRoleListOfUser");
-            body.addProperty("userName", username);
-            Object response = send("urn:getRoleListOfUser", body);
-            return getPrimitiveStringList(response);
-        }
+
+	public List<String> getRoleListOfUser(String username) throws SiteWhereException {
+		SoapObject body = new SoapObject(CARBON_NAMESPACE, "getRoleListOfUser");
+		body.addProperty("userName", username);
+		Object response = send("urn:getRoleListOfUser", body);
+		return getPrimitiveStringList(response);
+	}
 
 	/**
 	 * Send a SOAP message.
@@ -152,29 +151,30 @@ public class Wso2SoapClient {
 		}
 		throw new SiteWhereException("Invalid response from SOAP service.");
 	}
-        
-        /**
+
+	/**
 	 * Parse out a primitive String or a primitive String List response.
 	 * 
 	 * @param response
 	 * @return
 	 * @throws SiteWhereException
 	 */
-        protected List<String> getPrimitiveStringList(Object response) throws SiteWhereException {
-            LinkedList<String> values = new LinkedList<String>();
-            
-            if ((response != null) && (response instanceof SoapPrimitive)) {
-                String value = (String) ((SoapPrimitive) response).getValue();
-                values.add(value);
-            } else if ((response != null) && (response instanceof List)) {
-                List<SoapPrimitive> primitiveValues = (List<SoapPrimitive>)response;
-                for (SoapPrimitive primitiveValue : primitiveValues) {
-                    values.add((String)primitiveValue.getValue());
-                }
-            }
-            
-            return values;
-        }
+	@SuppressWarnings("unchecked")
+	protected List<String> getPrimitiveStringList(Object response) throws SiteWhereException {
+		LinkedList<String> values = new LinkedList<String>();
+
+		if ((response != null) && (response instanceof SoapPrimitive)) {
+			String value = (String) ((SoapPrimitive) response).getValue();
+			values.add(value);
+		} else if ((response != null) && (response instanceof List)) {
+			List<SoapPrimitive> primitiveValues = (List<SoapPrimitive>) response;
+			for (SoapPrimitive primitiveValue : primitiveValues) {
+				values.add((String) primitiveValue.getValue());
+			}
+		}
+
+		return values;
+	}
 
 	/**
 	 * Call to turn off SSL certificate checking. This should only be done for testing!
