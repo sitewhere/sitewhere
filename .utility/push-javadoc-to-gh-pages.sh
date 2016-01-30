300ef5b2cfd5c8ec1be086c6efe7a45a92986b74
Derek Adams <derek.adams@sitewhere.com>
2016-01-30 01:27:39 -0500

    Added utility script for TravisCI to publish Javadoc to website.
    Utility script automatically checks out sitewhere.github.io, copies in
    the latest Javadoc for api modules, then pushes the updates.

---
+++ .utility/push-javadoc-to-gh-pages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "sitewhere/sitewhere" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Publishing javadoc...\n"
+
+  cp -R sitewhere-core-api/build/docs/javadoc $HOME/javadoc-latest/sitewhere-core-api
+  cp -R sitewhere-server-api/build/docs/javadoc $HOME/javadoc-latest/sitewhere-server-api
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet https://${GH_TOKEN}@github.com/sitewhere/sitewhere.github.io sitewhere.github.io > /dev/null
+
+  cd website
+  git rm -rf ./javadoc
+  cp -Rf $HOME/javadoc-latest ./javadoc
+  git add -f .
+  git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to sitewhere.github.io."
+  git push -fq origin sitewhere.github.io > /dev/null
+
+  echo -e "Published Javadoc to sitewhere.github.io.\n"
+  
+fi
