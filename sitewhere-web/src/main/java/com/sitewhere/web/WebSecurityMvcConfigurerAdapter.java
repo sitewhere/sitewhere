660dfdc5a48e50790e2b0916d847094f35fa9075
Derek Adams <derek.adams@sitewhere.com>
2016-02-03 13:25:15 -0500

    Get rid of version numbers. Add unimplemented methods.

--- sitewhere-web/src/main/java/com/sitewhere/web/WebSecurityMvcConfigurerAdapter.java
+++ sitewhere-web/src/main/java/com/sitewhere/web/WebSecurityMvcConfigurerAdapter.java
@@ -19,10 +19,13 @@ import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -93,5 +96,21 @@ public class WebSecurityMvcConfigurerAdapter extends WebSecurityConfigurerAdapte
 	@Override
 	public Validator getValidator() {
 		return null;
+	}
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+	}
+
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
 	}
 }
