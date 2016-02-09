8128e8eb9ca5585a7aa7f746b13fbff916560205
Derek Adams <derek.adams@sitewhere.com>
2016-02-08 21:52:26 -0500

    Updated script to use correct variable name.

--- sitewhere-core/config/sitewhere/global/scripts/groovy/eventCreate.groovy
+++ sitewhere-core/config/sitewhere/global/scripts/groovy/eventCreate.groovy
@@ -8,6 +8,6 @@ if (event.hasMeasurement('fuel.level')) {
 	logger.info "Got fuel level. Creating a new location."
 	
 	// Create a new location event. It will be stored and processed like other events.
-	def newloc = eventsBuilder.newLocation 33.75, -84.39 withElevation 10 on(new Date()) trackState()
-	eventsBuilder.forSameAssignmentAs event persist newloc
+	def newloc = eventBuilder.newLocation 33.75, -84.39 withElevation 10 on(new Date()) trackState()
+	eventBuilder.forSameAssignmentAs event persist newloc
 }
