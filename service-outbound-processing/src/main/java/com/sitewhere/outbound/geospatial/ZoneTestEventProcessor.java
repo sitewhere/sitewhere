6ee6d0e0f659ba2530ebfb681dfb154efb571be5
Derek Adams <derek.adams@sitewhere.com>
2017-08-31 14:28:26 -0400

    Move zone matching to outbound processing microservice. #535

--- service-outbound-processing/src/main/java/com/sitewhere/outbound/geospatial/ZoneTestEventProcessor.java
+++ service-outbound-processing/src/main/java/com/sitewhere/outbound/geospatial/ZoneTestEventProcessor.java
@@ -5,7 +5,7 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package com.sitewhere.geospatial;
+package com.sitewhere.outbound.geospatial;
 
 import java.util.ArrayList;
 import java.util.Date;
