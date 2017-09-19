5f224c858c8931f9653b69c9df7ccc1d823306e4
Derek Adams <derek.adams@sitewhere.com>
2017-09-18 21:03:32 -0400

    Uncomment REST security checks. #535

--- service-web-rest/src/main/java/com/sitewhere/web/SiteWhereSecurity.java
+++ service-web-rest/src/main/java/com/sitewhere/web/SiteWhereSecurity.java
@@ -11,13 +11,16 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 import com.sitewhere.security.SitewhereAuthenticationProvider;
+import com.sitewhere.spi.user.SiteWhereRoles;
 
 @Configuration
 @EnableWebSecurity
@@ -56,11 +59,10 @@ public class SiteWhereSecurity extends WebSecurityConfigurerAdapter {
      * config. annotation.web.builders.HttpSecurity)
      */
     protected void configure(HttpSecurity http) throws Exception {
-	// http.csrf().disable();
-	// http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().antMatcher("/api/**")
-	// .authorizeRequests().antMatchers(HttpMethod.GET, "/").permitAll()
-	// .antMatchers(HttpMethod.OPTIONS,
-	// "/api/**").permitAll().antMatchers(HttpMethod.GET, "/api/**/symbol")
-	// .permitAll().antMatchers("/api/**").hasRole(SiteWhereRoles.AUTH_REST).and().httpBasic();
+	http.csrf().disable();
+	http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().antMatcher("/api/**")
+		.authorizeRequests().antMatchers(HttpMethod.GET, "/").permitAll()
+		.antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll().antMatchers(HttpMethod.GET, "/api/**/symbol")
+		.permitAll().antMatchers("/api/**").hasRole(SiteWhereRoles.AUTH_REST).and().httpBasic();
     }
 }
