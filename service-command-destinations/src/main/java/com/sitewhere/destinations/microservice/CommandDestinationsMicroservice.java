73f91646a078732347a4a80c462981e12e6f2b5f
Derek Adams <derek.adams@sitewhere.com>
2017-11-10 15:16:19 -0500

    Initial work on command destinations microservice. #535

---
+++ service-command-destinations/src/main/java/com/sitewhere/destinations/microservice/CommandDestinationsMicroservice.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) SiteWhere, LLC. All rights reserved. http://www.sitewhere.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package com.sitewhere.destinations.microservice;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.sitewhere.destinations.spi.microservice.ICommandDestinationsMicroservice;
+import com.sitewhere.destinations.spi.microservice.ICommandDestinationsTenantEngine;
+import com.sitewhere.microservice.multitenant.MultitenantMicroservice;
+import com.sitewhere.spi.SiteWhereException;
+import com.sitewhere.spi.tenant.ITenant;
+
+/**
+ * Microservice that provides command destinations functionality.
+ * 
+ * @author Derek
+ */
+public class CommandDestinationsMicroservice extends MultitenantMicroservice<ICommandDestinationsTenantEngine>
+	implements ICommandDestinationsMicroservice {
+
+    /** Static logger instance */
+    private static Logger LOGGER = LogManager.getLogger();
+
+    /** Microservice name */
+    private static final String NAME = "Command Destinations";
+
+    /** Identifies module resources such as configuration file */
+    private static final String MODULE_IDENTIFIER = "command-destinations";
+
+    /*
+     * @see com.sitewhere.spi.microservice.IMicroservice#getName()
+     */
+    @Override
+    public String getName() {
+	return NAME;
+    }
+
+    /*
+     * @see com.sitewhere.spi.microservice.IMicroservice#getIdentifier()
+     */
+    @Override
+    public String getIdentifier() {
+	return MODULE_IDENTIFIER;
+    }
+
+    /*
+     * @see com.sitewhere.spi.microservice.multitenant.IMultitenantMicroservice#
+     * createTenantEngine(com.sitewhere.spi.tenant.ITenant)
+     */
+    @Override
+    public ICommandDestinationsTenantEngine createTenantEngine(ITenant tenant) throws SiteWhereException {
+	return new CommandDestinationsTenantEngine(this, tenant);
+    }
+
+    /*
+     * @see com.sitewhere.spi.server.lifecycle.ILifecycleComponent#getLogger()
+     */
+    @Override
+    public Logger getLogger() {
+	return LOGGER;
+    }
+}
