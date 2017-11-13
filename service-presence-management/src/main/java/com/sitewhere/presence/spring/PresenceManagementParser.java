422eecdc0519ca01a6ffb1f655201382dee763a0
Derek Adams <derek.adams@sitewhere.com>
2017-11-13 12:55:23 -0500

    Added presence management microservice. #535

---
+++ service-presence-management/src/main/java/com/sitewhere/presence/spring/PresenceManagementParser.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) SiteWhere, LLC. All rights reserved. http://www.sitewhere.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package com.sitewhere.presence.spring;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.xml.AbstractBeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
+
+/**
+ * Parses elements related to device presence management.
+ * 
+ * @author Derek
+ */
+public class PresenceManagementParser extends AbstractBeanDefinitionParser {
+
+    /** Static logger instance */
+    @SuppressWarnings("unused")
+    private static Logger LOGGER = LogManager.getLogger();
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.springframework.beans.factory.xml.AbstractBeanDefinitionParser#
+     * parseInternal (org.w3c.dom.Element,
+     * org.springframework.beans.factory.xml.ParserContext)
+     */
+    @Override
+    protected AbstractBeanDefinition parseInternal(Element element, ParserContext context) {
+	return null;
+    }
+}
