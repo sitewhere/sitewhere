a95ded117818662a146b4a9a38c9131f8e182776
Derek Adams <derek.adams@sitewhere.com>
2017-11-13 17:42:24 -0500

    Add label generation microservice. #535

---
+++ service-label-generation/src/main/java/com/sitewhere/labels/spi/microservice/ILabelGenerationTenantEngine.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) SiteWhere, LLC. All rights reserved. http://www.sitewhere.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package com.sitewhere.labels.spi.microservice;
+
+import com.sitewhere.spi.microservice.multitenant.IMicroserviceTenantEngine;
+
+/**
+ * Extends {@link IMicroserviceTenantEngine} with features specific to label
+ * generation.
+ * 
+ * @author Derek
+ */
+public interface ILabelGenerationTenantEngine extends IMicroserviceTenantEngine {
+}
