4b521c450a3c4a8b10bc649f851c6a24c2fd57f8
Derek Adams <derek.adams@sitewhere.com>
2018-12-10 15:24:54 -0500

    Add license header.

--- sitewhere-grpc-client/src/main/java/com/sitewhere/grpc/client/batch/BatchModelMarshaler.java
+++ sitewhere-grpc-client/src/main/java/com/sitewhere/grpc/client/batch/BatchModelMarshaler.java
@@ -1,5 +1,12 @@
 package com.sitewhere.grpc.client.batch;
 
+/*
+ * Copyright (c) SiteWhere, LLC. All rights reserved. http://www.sitewhere.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
